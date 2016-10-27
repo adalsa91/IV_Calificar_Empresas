b6ecbf88ca35bf5e1faca2aa898ca1bb2216d532
Adrián Álvarez <adalsa91@gmail.com>
2016-10-27 10:59:40 +0200

    Añadidos tests unitarios para models.

--- calificaciones/tests.py
+++ calificaciones/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+from django.test import TestCase
+from calificaciones.models import Empresa
+from calificaciones.models import Alumno
+
+
+class EmpresaMethodTests(TestCase):
+
+    def test_empresa_creation(self):
+        emp = Empresa(nombre='Prueba')
+        emp.save()
+
+        empresas = Empresa.objects.all()
+        self.assertEqual(empresas.count(), 1)
+
+        self.assertEqual(emp.nombre, 'Prueba')
+        self.assertEqual(str(emp), emp.nombre)
+
+
+class AlumnoMethodTests(TestCase):
+
+    def test_alumno_creation(self):
+        emp = Empresa(nombre='Prueba')
+        emp.save()
+        alum = Alumno(nombre='John', empresa=emp, puntuacion='8')
+        alum.save()
+
+        alumnos = Alumno.objects.all()
+        self.assertEqual(alumnos.count(), 1)
+
+        self.assertEqual(alum.nombre, 'John')
+        self.assertEqual(alum.puntuacion, '8')
